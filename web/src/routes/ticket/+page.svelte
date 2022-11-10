fc50128aa3b9f6740383d7d17bb01858c05bdd7b
John Willman <johnpwillman@gmail.com>
2022-11-10 16:57:49 -0600

    Added md preview during ticket creation.

--- web/src/routes/ticket/+page.svelte
+++ web/src/routes/ticket/+page.svelte
@@ -1,4 +1,13 @@
 <script>
+    import { onMount } from 'svelte';
+
+    let md;
+    let textAreaVal='';
+
+    onMount(async () => {
+		md = new window.remarkable.Remarkable();
+	});
+
     /** @type {import('./$types').PageData} */
     export let data;
     console.log(data);
@@ -13,9 +22,15 @@
             <input type="text" name="subject" class="form-control" id="exampleFormControlInput1" placeholder="Quick description">
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-            <textarea name="body" class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Describe the issue in detail"></textarea>
+            <label for="exampleFormControlTextarea1" class="form-label">Ticket Body</label>
+            <textarea name="body" class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Describe the issue in detail (markdown allowed)" bind:value="{textAreaVal}"></textarea>
           </div>
+          {#if md && textAreaVal.length > 0}
+          <div class="mb-3">
+            <p><strong>Preview:</strong></p>
+            <p>{@html md.render(textAreaVal)}</p>
+          </div>
+          {/if}
         <div class="gap-2">
             <button class="btn btn-dark bg-indigo-400" type="submit">Submit Ticket</button>
         </div>
