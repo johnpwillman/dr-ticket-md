eeb7196d11dcc2953905a7d8e378cb9e7dbb8bf1
John Willman <johnpwillman@gmail.com>
2022-11-10 23:04:40 -0600

    Change ticket page to use bootstrap cards. Various css fixes for appearance.

--- web/src/routes/ticket/[slug]/+page.svelte
+++ web/src/routes/ticket/[slug]/+page.svelte
@@ -36,96 +36,104 @@
     <form method="POST" action="?/newComment">
     <div class="row mb-4 comment-form">
         <div class="col-md-8 comment-textarea">
-            <h4>Subject: {data.ticket.subject}</h4>
-            <div class="form mb-4">
-                <textarea class="form-control" name="body" rows="5" placeholder="Add a comment to this ticket." bind:value={textAreaVal}></textarea>
-            </div>
-            {#if md}
-            <div class="row">
-                <div class="col-md-3">
-                    <h4>Preview</h4>
+            <div class="card">
+                <h4 class="card-header">Ticket: "{data.ticket.subject}"</h4>
+                <div class="card-body">
+                    <textarea class="form-control" name="body" rows="5" placeholder="Add a comment to this ticket." bind:value={textAreaVal}></textarea>
                 </div>
-                <div class="col-md-9 markdown-preview">
+                {#if md}
+                <div class="card-footer">
+                    <p><strong>Preview:</strong></p>
                     <p>{@html md.render(textAreaVal)}</p>
                 </div>
+                {/if}
+                
             </div>
-            {/if}
         </div>
         <div class="col-md-4 comment-submit">
-            <h4>Status</h4>
-            <div class="row">
-                <div class="col-md-6">
-                    <select class="form-select comment-submit-select" name="status" id="exampleSelect" value="{data.ticket.status}">
-                        <option value="open">Open</option>
-                        <option value="pending">With Customer</option>
-                        <option value="closed">Closed</option>
-                    </select>
-                </div>
-                <div class="col-md-6">
-                    <button class="btn btn-secondary bg-indigo-400">Submit</button>
+            <div class="card">
+                <h4 class="card-header">Update</h4>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <select class="form-select comment-submit-select" name="status" id="exampleSelect" value="{data.ticket.status}">
+                                <option value="open">Open</option>
+                                <option value="pending">With Customer</option>
+                                <option value="closed">Closed</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <button class="btn btn-secondary bg-indigo-400">Submit</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     </form>
-    <div class="col-md-8 ticket-comments">
-        {#each data.comments as comment}
-        <div class="row mb-2">
-            <div class="col-md-3 pt-2 pb-2 text-center comment-info">
-                {#await get_gravatar_image_url(comment.submitted_by, 48)}
-                    <p>...</p>
-                {:then gravatar}
-                    <img class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="48" height="48" src="{gravatar}" alt="gravatar-img" />
-                {:catch error}
-                    <p style="color: red">{error.message}</p>
-                {/await}
-                <br/>
-                {comment.submitted_by}
-                <br/><br/>
-                {(new Date(comment.created_at)).toISOString().match(/(\d|-|:)+/g)[0]}
-                <br/>
-                {(new Date(comment.created_at)).toISOString().match(/(\d|-|:)+/g)[1]}
-            </div>
-            <div class="col-md-9 comment">
-                <div class="d-flex flex-column comment-column">
-                    <div class="comment-item">
-                        {#if md}
-                            {@html md.render(comment.body)}
-                        {:else}
-                            {comment.body}
-                        {/if}
+    <div class="row">
+        <div class="col-md-8 ticket-comments">
+            {#each data.comments as comment}
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-3 pt-2 pb-2 text-center comment-info">
+                        {#await get_gravatar_image_url(comment.submitted_by, 48)}
+                            <p>...</p>
+                        {:then gravatar}
+                            <img class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="48" height="48" src="{gravatar}" alt="gravatar-img" />
+                        {:catch error}
+                            <p style="color: red">{error.message}</p>
+                        {/await}
+                        <br/>
+                        {comment.submitted_by}
+                        <br/><br/>
+                        {(new Date(comment.created_at)).toISOString().match(/(\d|-|:)+/g)[0]}
+                        <br/>
+                        {(new Date(comment.created_at)).toISOString().match(/(\d|-|:)+/g)[1]}
                     </div>
-                    <div class="mt-auto state-change comment-item">
-                        Ticket state set to: {comment.status.toUpperCase()}
+                    <div class="col-md-9 d-flex align-items-start flex-column">
+                        <div class="card-body w-100">
+                            {#if md}
+                                <p>{@html md.render(comment.body)}</p>
+                            {:else}
+                                <p>{comment.body}</p>
+                            {/if}
+                        </div>
+                        <div class="card-footer w-100 text-muted">
+                            Ticket state set to: {comment.status.toUpperCase()}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        {/each}
-        <div class="row mb-2">
-            <div class="col-md-3 pt-2 p b-2 text-center comment-info">
-                {#await get_gravatar_image_url(data.ticket.submitted_by, 48)}
-                    <p>...</p>
-                {:then gravatar}
-                    <img class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="48" height="48" src="{gravatar}" alt="gravatar-img" />
-                {:catch error}
-                    <p style="color: red">{error.message}</p>
-                {/await}
-                <br />
-                {data.ticket.submitted_by}
-                <br/><br/>
-                {(new Date(data.ticket.created_at)).toISOString().split('T')[0]}
-                <br/>
-                {(new Date(data.ticket.created_at)).toISOString().replace('Z', '').split('T')[1]}
-            </div>
-            <div class="col-md-9 comment">
-                <div class="d-flex flex-column comment-column">
-                    <div class="comment-item">
-                        {#if md}
-                            {@html md.render(data.ticket.body)}
-                        {:else}
-                            {data.ticket.body}
-                        {/if}
+            {/each}
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-3 pt-2 pb-2 text-center comment-info">
+                        {#await get_gravatar_image_url(data.ticket.submitted_by, 48)}
+                            <p>...</p>
+                        {:then gravatar}
+                            <img class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="48" height="48" src="{gravatar}" alt="gravatar-img" />
+                        {:catch error}
+                            <p style="color: red">{error.message}</p>
+                        {/await}
+                        <br/>
+                        {data.ticket.submitted_by}
+                        <br/><br/>
+                        {(new Date(data.ticket.created_at)).toISOString().match(/(\d|-|:)+/g)[0]}
+                        <br/>
+                        {(new Date(data.ticket.created_at)).toISOString().match(/(\d|-|:)+/g)[1]}
+                    </div>
+                    <div class="col-md-9">
+                        <h5 class="card-header">
+                            {data.ticket.subject}
+                        </h5>
+                        <div class="card-body">
+                            {#if md}
+                                <p>{@html md.render(data.ticket.body)}</p>
+                            {:else}
+                                <p>{data.ticket.body}</p>
+                            {/if}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -145,10 +153,6 @@
     
     .bg-indigo-400 {
         background-color: #8540f5;
-    }
-
-    .comment-form {
-        background-color: lightgray;
     }
 
     /* .comment-submit-select {
