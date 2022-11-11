c27fb3bfd9c7664fa65cc91ec61f34eca1bfc32a
John Willman <johnpwillman@gmail.com>
2022-11-11 14:52:18 -0600

    combine form handling from server.js to page for new ticket route. remove server file.

--- web/src/routes/ticket/+page.svelte
+++ web/src/routes/ticket/+page.svelte
@@ -1,34 +1,65 @@
 <script>
-    import { onMount } from 'svelte';
+    import { onMount } from 'svelte'
+    import Cookies from 'js-cookie'
+    import { goto } from '$app/navigation';
 
-    let md;
-    let textAreaVal='';
+    let md
 
     onMount(async () => {
-		md = new window.remarkable.Remarkable();
+		md = new window.remarkable.Remarkable()
 	});
 
-    /** @type {import('./$types').PageData} */
-    export let data;
-    console.log(data);
+    let subject
+    let body
+    let apiRoot = `http://127.0.0.1:8000/v1/`;
+    async function newTicket() {
+        const status = 'open';
+
+        const response = await fetch(apiRoot + "tickets/" , {
+            method: "POST",
+            body: JSON.stringify({
+                subject: subject,
+                body: body,
+                status: status
+            }),
+            headers: {
+                "Authorization": "Bearer " + Cookies.get('jwt'),
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (response.ok) {
+            const ticket = await response.json();
+
+            goto(`/ticket/${ticket.key}`)
+        }
+
+        const respText = await response.text();
+        console.log(respText);
+
+        return {
+            success: response.ok,
+            message: respText
+        }
+    }
 </script>
 
 <main class="container">
-    <form method="POST" action="?/newTicket">
+    <form on:submit|preventDefault={newTicket}>
         <h1 class="h3 mb-3 fw-normal">Create a new Ticket</h1>
 
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Subject</label>
-            <input type="text" name="subject" class="form-control" id="exampleFormControlInput1" placeholder="Quick description">
+            <input bind:value="{subject}" type="text" name="subject" class="form-control" id="exampleFormControlInput1" placeholder="Quick description">
           </div>
           <div class="mb-3">
             <label for="exampleFormControlTextarea1" class="form-label">Ticket Body</label>
-            <textarea name="body" class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Describe the issue in detail (markdown allowed)" bind:value="{textAreaVal}"></textarea>
+            <textarea bind:value="{body}" name="body" class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Describe the issue in detail (markdown allowed)"></textarea>
           </div>
-          {#if md && textAreaVal.length > 0}
+          {#if md && body.length > 0}
           <div class="mb-3">
             <p><strong>Preview:</strong></p>
-            <p>{@html md.render(textAreaVal)}</p>
+            <p>{@html md.render(body)}</p>
           </div>
           {/if}
         <div class="gap-2">
