e8a5d546d4083d86dc89a975ea522ccd71be41f6
John Willman <johnpwillman@gmail.com>
2022-11-12 22:29:14 -0600

    Host SPA from npm run build from fastapi main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 origins = [
     "http://127.0.0.1",
@@ -26,6 +29,23 @@ app.include_router(v1_token.router)
 app.include_router(v1_users.router)
 app.include_router(v1_tickets.router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.mount("/_app", StaticFiles(directory="web/build/_app"), name="_app")
+app.mount("/static", StaticFiles(directory="web/build/static"), name="static")
+
+templates = Jinja2Templates(directory="web/build")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/ticket", response_class=HTMLResponse)
+async def new_ticket(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/ticket/{id}", response_class=HTMLResponse)
+async def ticket(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
