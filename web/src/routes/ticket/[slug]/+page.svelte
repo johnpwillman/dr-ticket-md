b78516cc0ea32e12a9da3c8e71e2b96b0bfa2376
John Willman <johnpwillman@gmail.com>
2022-11-14 15:34:32 -0600

    Move comment body items into components.

--- web/src/routes/ticket/[slug]/+page.svelte
+++ web/src/routes/ticket/[slug]/+page.svelte
@@ -11,6 +11,9 @@
     } from '$env/static/public';
     let apiRoot = PUBLIC_ENV == 'DEV' ? PUBLIC_DEV_API_ROOT : PUBLIC_API_ROOT
 
+    import CommentItem from './CommentItem.svelte'
+    import TicketCommentItem from './TicketCommentItem.svelte';
+
     let md
     let ticket
     let status
@@ -72,27 +75,6 @@
         await getTicket()
         body = ''
     }
-
-    async function md5 (string_to_hash) {
-        let response = await fetch('https://api.hashify.net/hash/md5/hex', {
-            method: "POST",
-            body: string_to_hash
-        });
-        let resJson = await response.json();
-        return resJson.Digest;
-    }
-
-    async function get_gravatar_image_url (email, size, default_image, allowed_rating, force_default)
-    {
-        email = typeof email !== 'undefined' ? email : 'john.doe@example.com';
-        size = (size >= 1 && size <= 2048) ? size : 80;
-        default_image = typeof default_image !== 'undefined' ? default_image : 'mm';
-        allowed_rating = typeof allowed_rating !== 'undefined' ? allowed_rating : 'g';
-        force_default = force_default === true ? 'y' : 'n';
-        let md5_email = await md5(email.toLowerCase().trim());
-        
-        return ("https://secure.gravatar.com/avatar/" + md5_email + "?size=" + size + "&default=" + encodeURIComponent(default_image) + "&rating=" + allowed_rating + (force_default === 'y' ? "&forcedefault=" + force_default : ''));
-    }
 </script>
 
 <main class="container">
@@ -138,69 +120,9 @@
     <div class="row">
         <div class="col-md-8 ticket-comments">
             {#each ticket.comments as comment}
-            <div class="card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3 pt-2 pb-2 text-center comment-info">
-                        {#await get_gravatar_image_url(comment.submitted_by, 48)}
-                            <p>...</p>
-                        {:then gravatar}
-                            <img class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="48" height="48" src="{gravatar}" alt="gravatar-img" />
-                        {:catch error}
-                            <p style="color: red">{error.message}</p>
-                        {/await}
-                        <br/>
-                        {comment.submitted_by}
-                        <br/><br/>
-                        {(new Date(comment.created_at)).toISOString().match(/(\d|-|:)+/g)[0]}
-                        <br/>
-                        {(new Date(comment.created_at)).toISOString().match(/(\d|-|:)+/g)[1]}
-                    </div>
-                    <div class="col-md-9 d-flex align-items-start flex-column">
-                        <div class="card-body w-100">
-                            {#if md}
-                                <p>{@html md.render(comment.body)}</p>
-                            {:else}
-                                <p>{comment.body}</p>
-                            {/if}
-                        </div>
-                        <div class="card-footer w-100 text-muted">
-                            Ticket state set to: {comment.status.toUpperCase()}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <CommentItem comment={comment} md={md} />
             {/each}
-            <div class="card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3 pt-2 pb-2 text-center comment-info">
-                        {#await get_gravatar_image_url(ticket.submitted_by, 48)}
-                            <p>...</p>
-                        {:then gravatar}
-                            <img class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="48" height="48" src="{gravatar}" alt="gravatar-img" />
-                        {:catch error}
-                            <p style="color: red">{error.message}</p>
-                        {/await}
-                        <br/>
-                        {ticket.submitted_by}
-                        <br/><br/>
-                        {(new Date(ticket.created_at)).toISOString().match(/(\d|-|:)+/g)[0]}
-                        <br/>
-                        {(new Date(ticket.created_at)).toISOString().match(/(\d|-|:)+/g)[1]}
-                    </div>
-                    <div class="col-md-9">
-                        <h5 class="card-header">
-                            {ticket.subject}
-                        </h5>
-                        <div class="card-body">
-                            {#if md}
-                                <p>{@html md.render(ticket.body)}</p>
-                            {:else}
-                                <p>{ticket.body}</p>
-                            {/if}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <TicketCommentItem ticket={ticket} md={md} />
         </div>
     </div>
     <div class="col-md-4 ticket-info">
@@ -214,17 +136,8 @@
         margin-top: 90px;
     }
 
-    
     .bg-indigo-400 {
         background-color: #8540f5;
-    }
-
-    /* .comment-submit-select {
-        width: 14em;
-    } */
-
-    .comment-info {
-        background-color: lightgray;
     }
     
 </style>
---
+++ web/src/routes/ticket/[slug]/CommentItem.svelte
@@ -0,0 +1,63 @@
+<script>
+    export let comment
+    export let md
+
+    async function md5 (string_to_hash) {
+        let response = await fetch('https://api.hashify.net/hash/md5/hex', {
+            method: "POST",
+            body: string_to_hash
+        })
+        let resJson = await response.json()
+        return resJson.Digest
+    }
+
+    async function get_gravatar_image_url (email, size, default_image, allowed_rating, force_default) {
+        email = typeof email !== 'undefined' ? email : 'john.doe@example.com'
+        size = (size >= 1 && size <= 2048) ? size : 80
+        default_image = typeof default_image !== 'undefined' ? default_image : 'mm'
+        allowed_rating = typeof allowed_rating !== 'undefined' ? allowed_rating : 'g'
+        force_default = force_default === true ? 'y' : 'n'
+        let md5_email = await md5(email.toLowerCase().trim())
+        
+        return ("https://secure.gravatar.com/avatar/" + md5_email + "?size=" + size + "&default=" + encodeURIComponent(default_image) + "&rating=" + allowed_rating + (force_default === 'y' ? "&forcedefault=" + force_default : ''))
+    }
+</script>
+
+
+<div class="card mb-3">
+    <div class="row g-0">
+        <div class="col-md-3 pt-2 pb-2 text-center comment-info">
+            {#await get_gravatar_image_url(comment.submitted_by, 48)}
+                <p>...</p>
+            {:then gravatar}
+                <img class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="48" height="48" src="{gravatar}" alt="gravatar-img" />
+            {:catch error}
+                <p style="color: red">{error.message}</p>
+            {/await}
+            <br/>
+            {comment.submitted_by}
+            <br/><br/>
+            {(new Date(comment.created_at)).toISOString().match(/(\d|-|:)+/g)[0]}
+            <br/>
+            {(new Date(comment.created_at)).toISOString().match(/(\d|-|:)+/g)[1]}
+        </div>
+        <div class="col-md-9 d-flex align-items-start flex-column">
+            <div class="card-body w-100">
+                {#if md}
+                    <p>{@html md.render(comment.body)}</p>
+                {:else}
+                    <p>{comment.body}</p>
+                {/if}
+            </div>
+            <div class="card-footer w-100 text-muted">
+                Ticket state set to: {comment.status.toUpperCase()}
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .comment-info {
+        background-color: lightgray;
+    }
+</style>
