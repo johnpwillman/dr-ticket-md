38256b4eac56bdb30cca2677cb907515e13cb5d5
John Willman <johnpwillman@gmail.com>
2022-11-16 10:07:40 -0600

    Add GET users/ route for retrieving all users in system. Add PATCH users/key route for patching a single user with new data.

--- api/v1/routers/users.py
+++ api/v1/routers/users.py
@@ -1,4 +1,4 @@
-import os
+from typing import List
 
 from fastapi import APIRouter, status, Depends, HTTPException
 
@@ -15,6 +15,21 @@ router = APIRouter(
 ###############################################################################
 # USER ADMINISTRATION
 ###############################################################################
+
+@router.get("/", response_model=List(User))
+async def all_users(current_user: User = Depends(get_current_active_user)):
+    if not current_user.admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User lacks permission for this route."
+        )
+    db = Base("dr-ticket-md-users")
+    res = db.fetch()
+    all_users: List(User) = res.items
+    while res.last:
+        res = db.fetch(last=res.last)
+        all_users.append(res.items)
+    return all_users
 
 @router.post("/")
 async def signup(signup: Signup):
@@ -34,6 +49,28 @@ async def signup(signup: Signup):
     )
     db.put(user.dict())
     return {"message": "User created"}
+
+@router.patch("/{key}", response_model=User)
+async def edit_user(key: str, user_patch: User, current_user: User = Depends(get_current_active_user)):
+    if not current_user.admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User lacks permission for this route."
+        )
+    if not key == user_patch.key:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Key from path does not match user being patched."
+        )
+    db = Base("dr-ticket-md-users")
+    try:
+        res = db.update(user_patch.dict(), key)
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="DB Error when updating user."
+        )
+    return user_patch
 
 @router.get("/me", response_model=User)
 async def my_user(current_user: User = Depends(get_current_active_user)):
